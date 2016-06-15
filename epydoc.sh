21e06f441f87473f502edf0f8a04871df0fba91d
Frédéric <fma@localhost>
2016-06-15 12:35:11 +0200

    Updated epydoc generated files path

--- epydoc.sh
+++ epydoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-epydoc -o ~/public_html/pKNyX -u http://www.pknyx.org -n pKNyX -v \
+epydoc -o /tmp/pKNyX -u http://www.pknyx.org -n pKNyX -v \
        --no-imports --show-frames --graph all --introspect-only \
        pknyx/common pknyx/core pknyx/plugins pknyx/services pknyx/stack pknyx/tools
 
