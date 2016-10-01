1a41617fd7500052a7a6a95f26b6284838517f25
Matthias Urlichs <matthias@urlichs.de>
2016-10-01 12:53:11 +0200

    fix dummy_2

--- pknyx/proto/dummy_2.py
+++ pknyx/proto/dummy_2.py
@@ -4,38 +4,56 @@ from __future__ import print_function
 
 from pprint import pprint
 
-from pknyx.api import Device, Stack, ETS
+from pknyx.api import Device, FunctionalBlock
+from pknyx.core.ets import ETS
 
-stack = Stack()   # Borg
-ets = ETS(stack)  # Borg
+ets = ETS()  # Borg
 
-class Dev(Device):
+class DevFB(FunctionalBlock):
 
     # Datapoints (= Group Objects) definition
-    DP_01 = dict(name="dp_1", dptId="1.001", flags="CWTU", priority="low", defaultValue=0.)
-    DP_02 = dict(name="dp_2", dptId="1.001", flags="CWTU", priority="low", defaultValue=0.)
-    DP_03 = dict(name="dp_3", dptId="1.001", flags="CWTU", priority="low", defaultValue=0.)
-    DP_04 = dict(name="dp_4", dptId="1.001", flags="CWTU", priority="low", defaultValue=0.)
+    DP_01 = dict(name="dp_1", dptId="1.001", default="Off", access="output")
+    DP_02 = dict(name="dp_2", dptId="1.001", default="Off", access="output")
+    DP_03 = dict(name="dp_3", dptId="1.001", default="Off", access="output")
+    DP_04 = dict(name="dp_4", dptId="1.001", default="Off", access="output")
+
+    GO_01 = dict(dp="dp_1", flags="CWTU", priority="low")
+    GO_02 = dict(dp="dp_2", flags="CWTU", priority="low")
+    GO_03 = dict(dp="dp_3", flags="CWTU", priority="low")
+    GO_04 = dict(dp="dp_4", flags="CWTU", priority="low")
 
     DESC = "Truc bidule"
 
-dev1 = Dev(name="dev1", desc="Device 1", address="1.1.1")
-dev2 = Dev(name="dev2", desc="Device 2", address="1.1.2")
+class Dev1(Device):
+    FB_01 = dict(cls=DevFB, name="dev_fb", desc="weather fb")
+
+    LNK_01 = dict(fb="dev_fb", dp="dp_1", gad="1/1/1")
+    LNK_01a = dict(fb="dev_fb", dp="dp_1", gad="2/1/1")
+    LNK_02 = dict(fb="dev_fb", dp="dp_2", gad="1/1/2")
+    LNK_03 = dict(fb="dev_fb", dp="dp_3", gad="1/1/3")
+    LNK_04 = dict(fb="dev_fb", dp="dp_4", gad="1/1/4")
+
+class Dev2(Device):
+    FB_01 = dict(cls=DevFB, name="dev_fb", desc="weather fb")
+
+    LNK_01 = dict(fb="dev_fb", dp="dp_1", gad="1/2/1")
+    LNK_02 = dict(fb="dev_fb", dp="dp_2", gad="1/2/2")
+    LNK_02a = dict(fb="dev_fb", dp="dp_2", gad="2/2/2")
+    LNK_03 = dict(fb="dev_fb", dp="dp_3", gad="1/1/3")
+    #LNK_04 = dict(fb="dev_fb", dp="dp_4", gad="1/1/4")
+
+dev1 = Dev1("1.1.1")
+dev2 = Dev2("1.1.2")
 
 ets.register(dev1)
 ets.register(dev2)
 
-ets.link(dev=dev1, dp="dp_1", gad=("1/1/1", "2/1/1"))
-ets.link(dev=dev1, dp="dp_2", gad="1/1/2")
-ets.link(dev=dev1, dp="dp_3", gad="1/1/3")
-ets.link(dev=dev1, dp="dp_4", gad="1/1/4")
-
-ets.link(dev=dev2, dp="dp_1", gad="1/2/1")
-ets.link(dev=dev2, dp="dp_2", gad=("1/2/2", "2/2/2"))
-ets.link(dev=dev2, dp="dp_3", gad="1/1/3")
+ets.link(dev1)
+ets.link(dev2)
 #ets.link(dev=dev2, dp="dp_4", gad="1/1/4")
 
-ets._gadName = {1: {'name': "heating",
+# TODO?
+ets._gadMap = {1: {'name': "heating",
                     1: {'name': "setpoint",
                         1: "living",
                         2: "bedroom 1",
