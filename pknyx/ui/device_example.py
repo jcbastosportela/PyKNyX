4432e2a7d5879abcfeebb363a5c2ef5104f729a7
Matthias Urlichs <matthias@urlichs.de>
2016-10-01 12:21:47 +0200

    there is no "toggle" module anywhere

--- pknyx/ui/device_example.py
+++ pknyx/ui/device_example.py
@@ -2,11 +2,13 @@
 
 from pknyx.api import Device
 
-from toggle import Toggle
+## TODO where does the "toggle" come from?
+
+#from toggle import Toggle
 
 
 class ServerDev(Device, xxx):
-    FB_01 = dict(cls=Toggle, name="toggle_wg", desc="Toggle widget")
+#    FB_01 = dict(cls=Toggle, name="toggle_wg", desc="Toggle widget")
 
     LNK_01 = dict(fb="toggle_wg", dp="cmd", gad="1/1/1")
     LNK_02 = dict(fb="toggle_wg", dp="state", gad="1/2/1")
